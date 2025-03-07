50c03bc6d23b4325104747b920c39d3e479f32cc
Nahidbd270 <Nahidbd270@gmail.com>
2025-03-07 21:34:01 +0600

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://envs.sh/E-p.jpg')).split()  # Sample pic
+PICS = (environ.get('PICS', 'https://i.ibb.co/9zHGyxB/photo-2025-03-07-15-33-30-7479091165459906572.jpg')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/60e8a622b14796e4448ce.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
